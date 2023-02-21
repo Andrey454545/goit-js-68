c43cab1fd49f8d2583746740541ec0e809f10a63
Andrey454545 <109917137+Andrey454545@users.noreply.github.com>
2023-02-21 21:46:59 +0200

    update index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello from Webpack');
+// console.log('Hello from Webpack');
+
+// запускаємо node src/index.js
+
+const express = require('express'); // імпорт в js прописують, як require
+const app = express();
+const port = 3000;      //- вказуємо на,  який порт все буде повішано пізніше
+
+app.get('/', (req, res) => {   // - слухаємо запит на '/'
+  res.send('<h1>Hello Andrey</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`) 
+})
+
